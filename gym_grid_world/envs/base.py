a1132d7b655318f6e8cde2863e47f2cc5952461a
Meteor <bobogei81123@gmail.com>
2017-06-26 20:31:01 +0800

    Implement render function

--- gym_grid_world/envs/base.py
+++ gym_grid_world/envs/base.py
@@ -3,8 +3,10 @@ import numpy as np
 import gym
 from gym import spaces
 from gym.utils import seeding
+from gym.envs.classic_control import rendering
 
 from PIL import Image, ImageDraw
+import pyglet
 
 class BaseEnv(gym.Env):
     '''
@@ -15,6 +17,8 @@ class BaseEnv(gym.Env):
     def __init__(self):
         self._seed()
         self.__configured = False
+
+        self.viewer = None
 
     def configure(self, actions, frame_size, *, max_step=-1):
         '''
@@ -77,9 +81,78 @@ class BaseEnv(gym.Env):
         return obs, rew, done, info
 
     def _render(self, mode='human', close=False):
-        pass
+        if close:
+            if self.viewer is not None:
+                self.viewer.close()
+                self.viewer = None
+            return
+
+        if self.viewer is None:
+            self.viewer = TKViewer(100, 100)
+
+        self.viewer.add_image(50, 50, self.image.resize((100, 100)))
 
     # utils functions
     def get_bitmap(self):
         arr = np.array(self.image).reshape((*self.frame_size, 3))
         return arr.astype('float32')
+
+import tkinter
+import threading
+from PIL import Image, ImageTk
+
+class TKViewer(threading.Thread):
+
+    def __init__(self, width, height):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.root = None
+        self.start()
+
+    def run(self):
+        self.root = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.root, width=self.width, height=self.height)
+        self.canvas.pack()
+        self.root.mainloop()
+
+    def close(self):
+        self.root.destroy()
+        self.root = None
+
+    def add_image(self, x, y, image):
+        if self.root is None:
+            return
+        try:
+            self.current_image = ImageTk.PhotoImage(image)
+            self.canvas.create_image(x, y, image=self.current_image)
+        except:
+            pass
+
+def numpy_to_PIL(rgb_array):
+    return Image.fromarray(rgb_array.astype(np.uint8), 'RGB')
+
+def numpy_to_pyglet(rgb_array):
+    width, height = rgb_array.shape[:2]
+    data = rgb_array.flatten()
+    return pyglet.image.ImageData(
+        width, height, 'RGB',
+        (pyglet.gl.GLubyte * data.size)(*data.astype('uint8')))
+
+class PygletImage(rendering.Geom):
+    def __init__(self, img):
+        rendering.Geom.__init__(self)
+        self.img = img
+        self.width = img.width
+        self.height = img.height
+        self.flip = False
+    def render(self):
+        x, y = 0, 0
+        w, h = self.width, self.height
+        for attr in self.attrs:
+            if type(attr) is rendering.Transform:
+                x += attr.translation[0]
+                y += attr.translation[1]
+                w *= attr.scale[0]
+                h *= attr.scale[1]
+        self.img.blit(-w/2+x, -h/2+y, width=w, height=h)
